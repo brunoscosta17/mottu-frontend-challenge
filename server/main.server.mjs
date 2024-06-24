449fb2224e674aa7214f2c7a4499eda7c22dad49
Bruno <brunoscosta17@gmail.com>
2024-06-24 16:07:29 -0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-U77GACEM.mjs";import"./chunk-3LJLWU44.mjs";import"./chunk-PQBD7P63.mjs";import"./chunk-5KYCKDE4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-MXLCTHH5.mjs";import"./chunk-3LJLWU44.mjs";import"./chunk-WL33CGEB.mjs";import"./chunk-5KYCKDE4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
